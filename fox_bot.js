f567b876cd4d74eecf040b146af0914baaf000f9
mari <sword20050@mail.ru>
2023-11-21 12:51:42 +0300

    add

--- fox_bot.js
+++ fox_bot.js
@@ -10,6 +10,6 @@ const bot = new TelegramBot(token, {polling: true});
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
   const mess = msg.text;
-
+//
   bot.sendMessage(chatId, mess);
 });
